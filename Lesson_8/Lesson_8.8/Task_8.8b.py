1a020fb1a649c78b6ffe8bfc3fb5935269ddb26f
Jlexa46 <lexa140191@gmail.com>
2022-04-01 23:11:24 +0300

    Complete Lesson_8

---
+++ Lesson_8/Lesson_8.8/Task_8.8b.py
@@ -0,0 +1,2 @@
+words = ['Plum', 'Grapefruit', 'apple', 'orange', 'pomegranate', 'Cranberry', 'lime', 'Lemon', 'grapes', 'persimmon', 'tangerine', 'Watermelon', 'currant', 'Almond']
+print(*sorted({w[0].lower() for w in words}))
---
+++ Lesson_8/Lesson_8.8/Task_8.8e.py
@@ -0,0 +1,3 @@
+files = ['python.png', 'qwerty.py', 'stepik.png', 'beegeek.org', 'windows.pnp', 'pen.txt', 'phone.py', 'book.txT', 'board.pNg', 'keyBoard.jpg', 'Python.PNg', 'apple.jpeg', 'png.png', 'input.tXt', 'split.pop', 'solution.Py', 'stepik.org', 'kotlin.ko', 'github.git']
+files = [f.lower() for f in files]
+print(*sorted({f for f in files if f[len(f)-3:len(f)] == 'png'}))
